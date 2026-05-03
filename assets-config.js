51e29c515e5c20245909a05f14618375539351c2
Han Yi <hvnyiteh@gmail.com>
2026-05-03 18:26:49 +0800

    add img

---
+++ assets-config.js
@@ -0,0 +1,1 @@
+window.ASSETS_BASE = 'https://oovdmtnwipgoumnrwmia.supabase.co/storage/v1/object/public/altoria-assets';
---
+++ products.js
@@ -0,0 +1,658 @@
+window.EMBEDDED_PRODUCTS = {
+  "diamonds": [
+    {
+      "id": "diamond-001",
+      "name": "Round Cut Necklace",
+      "type": "jewel",
+      "category": "necklaces",
+      "specs": "1.05 - 1.07 ct",
+      "description": "1.05 - 1.07 ct blah blah blah",
+      "badge": null,
+      "img": [
+        {
+          "id": "diamond-001",
+          "img_id": "diamond-001-1",
+          "img_addr": "diamond/21%20Necklace/F3003529-2.jpg"
+        },
+        {
+          "id": "diamond-001",
+          "img_id": "diamond-001-2",
+          "img_addr": "diamond/21%20Necklace/F3003543.jpg"
+        },
+        {
+          "id": "diamond-001",
+          "img_id": "diamond-001-3",
+          "img_addr": "diamond/21%20Necklace/F3003550.jpg"
+        }
+      ]
+    },
+    {
+      "id": "diamond-002",
+      "name": "Emerald Cut Ring",
+      "type": "jewel",
+      "category": "rings",
+      "specs": "3.55ct",
+      "description": "3.55ct",
+      "badge": null,
+      "img": [
+        {
+          "id": "diamond-002",
+          "img_id": "diamond-002-1",
+          "img_addr": "diamond/1%20Ring/LG652439541_1a.jpg"
+        },
+        {
+          "id": "diamond-002",
+          "img_id": "diamond-002-2",
+          "img_addr": "diamond/1%20Ring/LG652439541_2a.jpg"
+        },
+        {
+          "id": "diamond-002",
+          "img_id": "diamond-002-3",
+          "img_addr": "diamond/1%20Ring/LG652439541_3a.jpg"
+        }
+      ]
+    },
+    {
+      "id": "diamond-003",
+      "name": "Pear Cut Ring",
+      "type": "jewel",
+      "category": "rings",
+      "specs": "3.04ct",
+      "description": "3.04ct",
+      "badge": null,
+      "img": [
+        {
+            "id": "diamond-003",
+            "img_id": "diamond-003-1",
+            "img_addr": "diamond/2%20Ring/F3003583.jpg"
+          },
+          {
+            "id": "diamond-003",
+            "img_id": "diamond-003-2",
+            "img_addr": "diamond/2%20Ring/F3003589.jpg"
+          },
+          {
+            "id": "diamond-003",
+            "img_id": "diamond-003-3",
+            "img_addr": "diamond/2%20Ring/F3003591.jpg"
+          }
+      ]
+    },
+    {
+      "id": "diamond-004",
+      "name": "Pear Cut Ring",
+      "type": "jewel",
+      "category": "rings",
+      "specs": "3.20ct",
+      "description": "3.20ct",
+      "badge": null,
+      "img": [
+        {
+          "id": "diamond-004",
+          "img_id": "diamond-004-1",
+          "img_addr": "diamond/3%20Ring/F3003595.jpg"
+        },
+        {
+          "id": "diamond-004",
+          "img_id": "diamond-004-2",
+          "img_addr": "diamond/3%20Ring/F3003597.jpg"
+        },
+        {
+          "id": "diamond-004",
+          "img_id": "diamond-004-3",
+          "img_addr": "diamond/3%20Ring/F3003600.jpg"
+        }
+      ]
+    },
+    {
+      "id": "diamond-005",
+      "name": "Round Cut (Pair) Earrings",
+      "type": "jewel",
+      "category": "earrings",
+      "specs": "10.14ct",
+      "description": "10.14ct",
+      "badge": null,
+      "img": [
+        {
+          "id": "diamond-005",
+          "img_id": "diamond-005-1",
+          "img_addr": "diamond/4%20Earring/LG70477276_1.jpg"
+        },
+        {
+          "id": "diamond-005",
+          "img_id": "diamond-005-2",
+          "img_addr": "diamond/4%20Earring/LG70477276_2.jpg"
+        },
+        {
+          "id": "diamond-005",
+          "img_id": "diamond-005-3",
+          "img_addr": "diamond/4%20Earring/LG70477276_3.jpg"
+        }
+      ]
+    },
+    {
+      "id": "diamond-006",
+      "name": "Princess Cut Pendant Necklace",
+      "type": "jewel",
+      "category": "necklaces",
+      "specs": "2.03ct",
+      "description": "2.03ct",
+      "badge": null,
+      "img": [
+        {
+          "id": "diamond-006",
+          "img_id": "diamond-006-1",
+          "img_addr": "diamond/5%20Necklace/F3003797.jpg"
+        },
+        {
+          "id": "diamond-006",
+          "img_id": "diamond-006-2",
+          "img_addr": "diamond/5%20Necklace/F3003800.jpg"
+        },
+        {
+          "id": "diamond-006",
+          "img_id": "diamond-006-3",
+          "img_addr": "diamond/5%20Necklace/F3003804.jpg"
+        }
+      ]
+    },
+    {
+      "id": "diamond-007",
+      "name": "Heart Cut Ring",
+      "type": "jewel",
+      "category": "rings",
+      "specs": "3.01ct",
+      "description": "3.01ct",
+      "badge": null,
+      "img": [
+        {
+          "id": "diamond-007",
+          "img_id": "diamond-007-1",
+          "img_addr": "diamond/6%20Ring/F3003603.jpg"
+        },
+        {
+          "id": "diamond-007",
+          "img_id": "diamond-007-2",
+          "img_addr": "diamond/6%20Ring/F3003605.jpg"
+        },
+        {
+          "id": "diamond-007",
+          "img_id": "diamond-007-3",
+          "img_addr": "diamond/6%20Ring/F3003609.jpg"
+        }
+      ]
+    },
+    {
+      "id": "diamond-008",
+      "name": "Emerald Cut Ring",
+      "type": "jewel",
+      "category": "rings",
+      "specs": "5.06ct",
+      "description": "5.06ct",
+      "badge": null,
+      "img": [
+        {
+          "id": "diamond-008",
+          "img_id": "diamond-008-3",
+          "img_addr": "diamond/7%20Ring/F3003620.jpg"
+        },
+        {
+          "id": "diamond-008",
+          "img_id": "diamond-008-1",
+          "img_addr": "diamond/7%20Ring/F3003614.jpg"
+        },
+        {
+          "id": "diamond-008",
+          "img_id": "diamond-008-2",
+          "img_addr": "diamond/7%20Ring/F3003615.jpg"
+        }
+      ]
+    },
+    {
+      "id": "diamond-009",
+      "name": "Round Cut Tennis Bracelet",
+      "type": "jewel",
+      "category": "bracelets",
+      "specs": "11.08ct",
+      "description": "11.08ct",
+      "badge": null,
+      "img": [
+        {
+          "id": "diamond-009",
+          "img_id": "diamond-009-1",
+          "img_addr": "diamond/8%20Bracelet/F3003518.jpg"
+        },
+        {
+          "id": "diamond-009",
+          "img_id": "diamond-009-2",
+          "img_addr": "diamond/8%20Bracelet/F3003520.jpg"
+        },
+        {
+          "id": "diamond-009",
+          "img_id": "diamond-009-3",
+          "img_addr": "diamond/8%20Bracelet/F3003524.jpg"
+        }
+      ]
+    },
+    {
+      "id": "diamond-010",
+      "name": "Round Cut Tennis Bracelet",
+      "type": "jewel",
+      "category": "bracelets",
+      "specs": "9.00ct",
+      "description": "9.00ct",
+      "badge": null,
+      "img": [
+        {
+          "id": "diamond-010",
+          "img_id": "diamond-010-1",
+          "img_addr": "diamond/9%20Bracelet/F3003782.jpg"
+        },
+        {
+          "id": "diamond-010",
+          "img_id": "diamond-010-2",
+          "img_addr": "diamond/9%20Bracelet/F3003785.jpg"
+        },
+        {
+          "id": "diamond-010",
+          "img_id": "diamond-010-3",
+          "img_addr": "diamond/9%20Bracelet/F3003786.jpg"
+        }
+      ]
+    },
+    {
+      "id": "diamond-011",
+      "name": "Round Cut Half Eternity Ring",
+      "type": "jewel",
+      "category": "rings",
+      "specs": "3.50ct",
+      "description": "3.50ct",
+      "badge": null,
+      "img": [
+        {
+          "id": "diamond-011",
+          "img_id": "diamond-011-1",
+          "img_addr": "diamond/10%20Ring/F3003636.jpg"
+        },
+        {
+          "id": "diamond-011",
+          "img_id": "diamond-011-2",
+          "img_addr": "diamond/10%20Ring/F3003637.jpg"
+        },
+        {
+          "id": "diamond-011",
+          "img_id": "diamond-011-3",
+          "img_addr": "diamond/10%20Ring/F3003639.jpg"
+        }
+      ]
+    },
+    {
+      "id": "diamond-012",
+      "name": "Round Cut Half Eternity Ring",
+      "type": "jewel",
+      "category": "rings",
+      "specs": "5.68ct",
+      "description": "5.68ct",
+      "badge": null,
+      "img": [
+        {
+          "id": "diamond-012",
+          "img_id": "diamond-012-1",
+          "img_addr": "diamond/11%20Ring/F3003646.jpg"
+        },
+        {
+          "id": "diamond-012",
+          "img_id": "diamond-012-2",
+          "img_addr": "diamond/11%20Ring/F3003650.jpg"
+        },
+        {
+          "id": "diamond-012",
+          "img_id": "diamond-012-3",
+          "img_addr": "diamond/11%20Ring/F3003655.jpg"
+        }
+      ]
+    },
+    {
+      "id": "diamond-013",
+      "name": "Emerald Cut Full Eternity Ring",
+      "type": "jewel",
+      "category": "rings",
+      "specs": "7.50ct",
+      "description": "7.50ct",
+      "badge": null,
+      "img": [
+        {
+          "id": "diamond-013",
+          "img_id": "diamond-013-1",
+          "img_addr": "diamond/12%20Ring/F3003665.jpg"
+        },
+        {
+          "id": "diamond-013",
+          "img_id": "diamond-013-2",
+          "img_addr": "diamond/12%20Ring/F3003670.jpg"
+        },
+        {
+          "id": "diamond-013",
+          "img_id": "diamond-013-3",
+          "img_addr": "diamond/12%20Ring/F3003672.jpg"
+        }
+      ]
+    },
+    {
+      "id": "diamond-014",
+      "name": "Butterflies Earrings",
+      "type": "jewel",
+      "category": "earrings",
+      "specs": "3.60ct",
+      "description": "3.60ct",
+      "badge": null,
+      "img": [
+        {
+          "id": "diamond-014",
+          "img_id": "diamond-014-1",
+          "img_addr": "diamond/13%20Earring/F3003694.jpg"
+        },
+        {
+          "id": "diamond-014",
+          "img_id": "diamond-014-2",
+          "img_addr": "diamond/13%20Earring/F3003702.jpg"
+        },
+        {
+          "id": "diamond-014",
+          "img_id": "diamond-014-3",
+          "img_addr": "diamond/13%20Earring/F3003700.jpg"
+        }
+      ]
+    },
+    {
+      "id": "diamond-015",
+      "name": "Round Cut Full Eternity Ring",
+      "type": "jewel",
+      "category": "rings",
+      "specs": "2.02ct",
+      "description": "2.02ct",
+      "badge": null,
+      "img": [
+        {
+          "id": "diamond-015",
+          "img_id": "diamond-015-1",
+          "img_addr": "diamond/14%20Ring/F3003660.jpg"
+        },
+        {
+          "id": "diamond-015",
+          "img_id": "diamond-015-2",
+          "img_addr": "diamond/14%20Ring/F3003661.jpg"
+        },
+        {
+          "id": "diamond-015",
+          "img_id": "diamond-015-3",
+          "img_addr": "diamond/14%20Ring/F3003664.jpg"
+        }
+      ]
+    },
+    {
+      "id": "diamond-016",
+      "name": "Pear Cut Pendant Necklace",
+      "type": "jewel",
+      "category": "necklaces",
+      "specs": "1.50ct",
+      "description": "1.50ct",
+      "badge": null,
+      "img": [
+        {
+          "id": "diamond-016",
+          "img_id": "diamond-016-1",
+          "img_addr": "diamond/15%20Necklace/F3003815.jpg"
+        },
+        {
+          "id": "diamond-016",
+          "img_id": "diamond-016-2",
+          "img_addr": "diamond/15%20Necklace/F3003820.jpg"
+        },
+        {
+          "id": "diamond-016",
+          "img_id": "diamond-016-3",
+          "img_addr": "diamond/15%20Necklace/F3003810.jpg"
+        },
+        {
+          "id": "diamond-016",
+          "img_id": "diamond-016-4",
+          "img_addr": "diamond/15%20Necklace/F3003794.jpg"
+        }
+      ]
+    },
+    {
+      "id": "diamond-017",
+      "name": "Flower Earrings",
+      "type": "jewel",
+      "category": "earrings",
+      "specs": "6.16ct",
+      "description": "6.16ct",
+      "badge": null,
+      "img": [
+        {
+          "id": "diamond-017",
+          "img_id": "diamond-017-1",
+          "img_addr": "diamond/16%20Earring/F3003712.jpg"
+        },
+        {
+          "id": "diamond-017",
+          "img_id": "diamond-017-2",
+          "img_addr": "diamond/16%20Earring/F3003718.jpg"
+        },
+        {
+          "id": "diamond-017",
+          "img_id": "diamond-017-3",
+          "img_addr": "diamond/16%20Earring/F3003723.jpg"
+        }
+      ]
+    },
+    {
+      "id": "diamond-018",
+      "name": "Round Cut (Pair) Earrings",
+      "type": "jewel",
+      "category": "earrings",
+      "specs": "6.01ct",
+      "description": "6.01ct",
+      "badge": null,
+      "img": [
+        {
+          "id": "diamond-018",
+          "img_id": "diamond-018-1",
+          "img_addr": "diamond/17%20Earring/F3003764.jpg"
+        },
+        {
+          "id": "diamond-018",
+          "img_id": "diamond-018-2",
+          "img_addr": "diamond/17%20Earring/F3003766.jpg"
+        },
+        {
+          "id": "diamond-018",
+          "img_id": "diamond-018-3",
+          "img_addr": "diamond/17%20Earring/F3003731.jpg"
+        },
+        {
+          "id": "diamond-018",
+          "img_id": "diamond-018-4",
+          "img_addr": "diamond/17%20Earring/F3003733.jpg"
+        }
+      ]
+    },
+    {
+      "id": "diamond-019",
+      "name": "Round Cut Hoop Earrings",
+      "type": "jewel",
+      "category": "earrings",
+      "specs": "5.76ct",
+      "description": "5.76ct",
+      "badge": null,
+      "img": [
+        {
+          "id": "diamond-019",
+          "img_id": "diamond-019-1",
+          "img_addr": "diamond/18%20Earring/F3003735.jpg"
+        },
+        {
+          "id": "diamond-019",
+          "img_id": "diamond-019-2",
+          "img_addr": "diamond/18%20Earring/F3003743.jpg"
+        }
+      ]
+    },
+    {
+      "id": "diamond-020",
+      "name": "Oval Cut Ring",
+      "type": "jewel",
+      "category": "rings",
+      "specs": "5.08ct",
+      "description": "5.08ct",
+      "badge": null,
+      "img": [
+        {
+          "id": "diamond-020",
+          "img_id": "diamond-020-1",
+          "img_addr": "diamond/19%20Ring/F3003674.jpg"
+        },
+        {
+          "id": "diamond-020",
+          "img_id": "diamond-020-2",
+          "img_addr": "diamond/19%20Ring/F3003678.jpg"
+        },
+        {
+          "id": "diamond-020",
+          "img_id": "diamond-020-3",
+          "img_addr": "diamond/19%20Ring/F3003682.jpg"
+        }
+      ]
+    },
+    {
+      "id": "diamond-021",
+      "name": "Round Cut (Pair) Earrings",
+      "type": "jewel",
+      "category": "earrings",
+      "specs": "2.00ct",
+      "description": "2.00ct",
+      "badge": null,
+      "img": [
+        {
+          "id": "diamond-021",
+          "img_id": "diamond-021-1",
+          "img_addr": "diamond/20%20Earring/F3003773.jpg"
+        },
+        {
+          "id": "diamond-021",
+          "img_id": "diamond-021-2",
+          "img_addr": "diamond/20%20Earring/F3003777.jpg"
+        },
+        {
+          "id": "diamond-021",
+          "img_id": "diamond-021-3",
+          "img_addr": "diamond/20%20Earring/F3003751.jpg"
+        },
+        {
+          "id": "diamond-021",
+          "img_id": "diamond-021-4",
+          "img_addr": "diamond/20%20Earring/F3003756.jpg"
+        }
+      ]
+    }
+  ],
+  "skincare": [
+    {
+      "id": "skincare-001",
+      "name": "Luminance Serum",
+      "type": "skincare",
+      "category": "serums",
+      "specs": "Brightening vitamin C complex. Lightweight, fast-absorbing.",
+      "description": "Brightening vitamin C complex. Lightweight, fast-absorbing.",
+      "badge": "New In",
+      "img": [
+        {
+          "id": "skincare-001",
+          "img_id": "skincare-001-1",
+          "img_addr": ""
+        }
+      ]
+    },
+    {
+      "id": "skincare-002",
+      "name": "Veil Cream",
+      "type": "skincare",
+      "category": "moisturisers",
+      "specs": "24-hour hydration barrier. Fragrance-free, for sensitive skin.",
+      "description": "24-hour hydration barrier. Fragrance-free, for sensitive skin.",
+      "badge": null,
+      "img": [
+        {
+          "id": "skincare-002",
+          "img_id": "skincare-002-1",
+          "img_addr": ""
+        }
+      ]
+    },
+    {
+      "id": "skincare-003",
+      "name": "Purity Gel",
+      "type": "skincare",
+      "category": "cleansers",
+      "specs": "Gentle foaming cleanser. Removes impurities without stripping.",
+      "description": "Gentle foaming cleanser. Removes impurities without stripping.",
+      "badge": "Bestseller",
+      "img": [
+        {
+          "id": "skincare-003",
+          "img_id": "skincare-003-1",
+          "img_addr": ""
+        }
+      ]
+    },
+    {
+      "id": "skincare-004",
+      "name": "Starter Ritual",
+      "type": "skincare",
+      "category": "sets",
+      "specs": "A curated 3-step morning and evening routine.",
+      "description": "A curated 3-step morning and evening routine.",
+      "badge": null,
+      "img": [
+        {
+          "id": "skincare-004",
+          "img_id": "skincare-004-1",
+          "img_addr": ""
+        }
+      ]
+    },
+    {
+      "id": "skincare-005",
+      "name": "Repair Ampoule",
+      "type": "skincare",
+      "category": "serums",
+      "specs": "Overnight recovery treatment. Fragrance-free, post-procedure safe.",
+      "description": "Overnight recovery treatment. Fragrance-free, post-procedure safe.",
+      "badge": "New In",
+      "img": [
+        {
+          "id": "skincare-005",
+          "img_id": "skincare-005-1",
+          "img_addr": ""
+        }
+      ]
+    },
+    {
+      "id": "skincare-006",
+      "name": "Dew Shield SPF",
+      "type": "skincare",
+      "category": "moisturisers",
+      "specs": "Lightweight SPF 50 daily moisturiser. No white cast.",
+      "description": "Lightweight SPF 50 daily moisturiser. No white cast.",
+      "badge": null,
+      "img": [
+        {
+          "id": "skincare-006",
+          "img_id": "skincare-006-1",
+          "img_addr": ""
+        }
+      ]
+    }
+  ]
+};
